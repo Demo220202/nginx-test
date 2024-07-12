d25d691351b640216d28603abafb3974cc906d78
Demo220202 <158024374+Demo220202@users.noreply.github.com>
2024-07-12 14:02:13 +0530

    Create beta-deploy.sh

---
+++ conf-sh/beta-deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo 'Hello World'
+
+x1="beta-${1}.conf"
+x2="beta-${1}"
+s_name="app.site.com.conf"
+s_url="app.site.com"
+file_path="/etc/nginx/sites-enabled/"
+
+echo "x1: $x1"
+echo "x2: $x2"
+echo "s_name: $s_name"
+echo "s_url: $s_url"
+echo "file_path: $file_path"
+
+# Copy the file with the name x
+cd $file_path
+sudo cp $s_name $x1
+
+# Edit the text inside the file
+sudo sed -i "s/$s_url/$x2/g" "$x1"
+
+sudo nginx -t
+
+sudo systemctl restart nginx
+sudo systemctl status nginx
+
+# Save the changes
+echo "Changes applied successfully."
